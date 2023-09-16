b84e173fb419ba3cdc91ac168b88f0265cbdfa28
williams.barriquero <williamsgomess@outlook.com>
2023-09-16 00:14:37 -0300

    3.6. Mapeando o id da entidade para autoincremento

--- src/main/java/br/com/williamsbarriquero/algafood/domain/model/Cozinha.java
+++ src/main/java/br/com/williamsbarriquero/algafood/domain/model/Cozinha.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -15,6 +17,7 @@ public class Cozinha {
 
     @EqualsAndHashCode.Include
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
--- src/main/java/br/com/williamsbarriquero/algafood/domain/model/Restaurante.java
+++ src/main/java/br/com/williamsbarriquero/algafood/domain/model/Restaurante.java
@@ -4,9 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Getter
@@ -17,6 +15,7 @@ public class Restaurante {
 
     @EqualsAndHashCode.Include
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
