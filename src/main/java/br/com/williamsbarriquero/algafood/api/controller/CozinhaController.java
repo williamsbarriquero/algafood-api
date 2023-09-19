e8a13cf251ce0f3ac764acc2312479c74f9afd68
williams.barriquero <williamsgomess@outlook.com>
2023-09-19 01:11:25 -0300

    4.14. Consultando Singleton Resource com GET e @PathVariable 4.23. Modelando e implementando a inclusão de recursos com POST

--- src/main/java/br/com/williamsbarriquero/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/williamsbarriquero/algafood/api/controller/CozinhaController.java
@@ -2,9 +2,9 @@ package br.com.williamsbarriquero.algafood.api.controller;
 
 import br.com.williamsbarriquero.algafood.domain.model.Cozinha;
 import br.com.williamsbarriquero.algafood.domain.repository.CozinhaRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,20 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+        if (cozinha != null) return ResponseEntity.ok(cozinha);
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
     }
 }
