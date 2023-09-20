e76eec40609597aef669fc5c47ad47d5c57d1883
williams.barriquero <williamsgomess@outlook.com>
2023-09-20 15:23:12 -0300

    5.10. Externalizando consultas JPQL para um arquivo XML

--- src/main/java/br/com/williamsbarriquero/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/br/com/williamsbarriquero/algafood/domain/repository/CozinhaRepository.java
@@ -3,5 +3,14 @@ package br.com.williamsbarriquero.algafood.domain.repository;
 import br.com.williamsbarriquero.algafood.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
+
+    List<Cozinha> findTodasByNomeContaining(String nome);
+
+    Optional<Cozinha> findByNome(String nome);
+
+    boolean existsByNome(String nome);
 }
--- src/main/java/br/com/williamsbarriquero/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/br/com/williamsbarriquero/algafood/domain/repository/RestauranteRepository.java
@@ -2,8 +2,26 @@ package br.com.williamsbarriquero.algafood.domain.repository;
 
 import br.com.williamsbarriquero.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+    List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+    //	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+
+//	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+
+    Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+
+    List<Restaurante> findTop2ByNomeContaining(String nome);
+
+    int countByCozinhaId(Long cozinha);
 }
