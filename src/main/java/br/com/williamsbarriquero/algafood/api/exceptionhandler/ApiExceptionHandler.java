c9c6477b795ff292bee142cc25986fc3cafe930b
Williams Barriquero <williamsgomess@outlook.com>
2023-10-08 00:15:24 -0300

    Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- src/main/java/br/com/williamsbarriquero/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/williamsbarriquero/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -60,6 +61,13 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         ProblemType problemType = ProblemType.DADOS_INVALIDOS;
         String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
+
+        BindingResult bindingResult = ex.getBindingResult();
+
+        List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
+                .map(fieldError -> Problem.Field.builder()
+                        .name(fieldError.getField())
+                        .userMessage(fieldError.getDefaultMessage()).build()).toList();
 
         Problem problem = createProblemBuilder(status, problemType, detail)
                 .userMessage(detail)
