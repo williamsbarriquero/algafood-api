8967ce088c68a3b8199a98069034008514e0ec27
Williams Barriquero <williamsgomess@outlook.com>
2023-10-10 01:53:00 -0300

    10.7. Configurando Maven Failsafe Plugin no projeto

--- src/test/java/br/com/williamsbarriquero/algafood/CadastroCozinhaIT.java
+++ src/test/java/br/com/williamsbarriquero/algafood/CadastroCozinhaIT.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-class CadastroCozinhaIntegrationTest {
+class CadastroCozinhaIT {
 
     @Autowired
     private CadastroCozinhaService cadastroCozinha;
