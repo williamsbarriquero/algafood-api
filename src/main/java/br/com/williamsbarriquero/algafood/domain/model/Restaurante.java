828afd3218e590cb7c7493ff52bea8d147f9187f
Williams Barriquero <williamsgomess@outlook.com>
2023-10-10 00:35:09 -0300

    9.17. Criando constraints de validação customizadas em nível de classe

--- src/main/java/br/com/williamsbarriquero/algafood/domain/model/Restaurante.java
+++ src/main/java/br/com/williamsbarriquero/algafood/domain/model/Restaurante.java
@@ -2,6 +2,7 @@ package br.com.williamsbarriquero.algafood.domain.model;
 
 import br.com.williamsbarriquero.algafood.core.validation.Groups;
 import br.com.williamsbarriquero.algafood.core.validation.Multiplo;
+import br.com.williamsbarriquero.algafood.core.validation.ValorZeroIncluiDescricao;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +21,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+
+@ValorZeroIncluiDescricao(valorField = "taxaFrete",
+        descricaoField = "nome", descricaoObrigatoria = "Frete Grátis")
 @Getter
 @Setter
 @Entity
@@ -35,7 +39,7 @@ public class Restaurante {
 
     @NotNull
     @PositiveOrZero
-    @Multiplo()
+    @Multiplo(numero = 5)
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
