ff71e6df50bbb5aad2605e8130eec7850d238bfe
Williams Barriquero <williamsgomess@outlook.com>
2023-10-16 01:03:31 -0300

    11.16. Customizando o mapeamento de propriedades com ModelMapper

--- src/main/java/br/com/williamsbarriquero/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/br/com/williamsbarriquero/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package br.com.williamsbarriquero.algafood.core.modelmapper;
 
+import br.com.williamsbarriquero.algafood.api.model.RestauranteModel;
+import br.com.williamsbarriquero.algafood.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,11 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        final var modelMapper = new ModelMapper();
+
+        modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+                .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+
+        return modelMapper;
     }
 }
